cb8fd8932fc67f2c5b36b8ad7d8a83a7cbe0a06e
Xiao Ling <lingxiao1002@gmail.com>
2022-03-09 16:51:03 +0800

    support qr detection

--- ios/Classes/FLNativeView.swift
+++ ios/Classes/FLNativeView.swift
@@ -1,15 +1,12 @@
 import Flutter
 import UIKit
-import DynamsoftBarcodeReader
 import DynamsoftCameraEnhancer
 
-class FLNativeView: NSObject, FlutterPlatformView, DBRTextResultDelegate {
+class FLNativeView: NSObject, FlutterPlatformView, DetectionHandler {
     private var _view: UIView
-    private var dceView: DCECameraView! = nil
-    private var barcodeReader: DynamsoftBarcodeReader! = nil
-    private var dce: DynamsoftCameraEnhancer! = nil
     private var messenger: FlutterBinaryMessenger
     private var channel: FlutterMethodChannel
+    private var qrCodeScanner: FLQRCodeScanner
     init(
         frame: CGRect,
         viewIdentifier viewId: Int64,
@@ -17,67 +14,47 @@ class FLNativeView: NSObject, FlutterPlatformView, DBRTextResultDelegate {
         binaryMessenger: FlutterBinaryMessenger
     ) {
         self.messenger = binaryMessenger
-        dceView = DCECameraView.init(frame: frame)
-        dce = DynamsoftCameraEnhancer.init(view: dceView)
+        let cameraView = DCECameraView.init(frame: frame)
+        let dce = DynamsoftCameraEnhancer.init(view: cameraView)
         dce.open()
         dce.setFrameRate(30)
-        _view = dceView
+        _view = cameraView
+
+        qrCodeScanner = FLQRCodeScanner.init(cameraView: cameraView, dce: dce)
 
         channel = FlutterMethodChannel(name: "com.dynamsoft.flutter_camera_qrcode_scanner/nativeview_" + String(viewId), binaryMessenger: messenger)
+        
+        super.init()
+
+        qrCodeScanner.setDetectionHandler(handler: self)
         channel.setMethodCallHandler({
         (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
             switch call.method {
                 case "startScanning":
-                    print("startScanning")
+                    self.qrCodeScanner.startScan()
                     result(.none)
                 case "stopScanning":
-                    print("stopScanning")
+                    self.qrCodeScanner.stopScan()
                     result(.none)
                 case "setLicense":
-                    print("setLicense")
+                    self.qrCodeScanner.setLicense(license: (call.arguments as! NSDictionary).value(forKey: "license") as! String)
                     result(.none)
                 case "setBarcodeFormats":
-                    print("setBarcodeFormats")
+                    self.qrCodeScanner.setBarcodeFormats(arg: call.arguments as! NSDictionary)
                     result(.none)
                 default:
                     result(.none)
                 }
         })
-        super.init()
-
-        createBarcodeReader(dce: dce)
     }
 
     func view() -> UIView {
         return _view
     }
 
-    func createBarcodeReader(dce: DynamsoftCameraEnhancer) {
-        // To activate the sdk, apply for a license key: https://www.dynamsoft.com/customer/license/trialLicense?product=dbr
-        barcodeReader = DynamsoftBarcodeReader.init(license: "license-key")
-        barcodeReader.setCameraEnhancer(dce)
-
-        // Set text result call back to get barcode results.
-        barcodeReader.setDBRTextResultDelegate(self, userData: nil)
-
-        // Start the barcode decoding thread.
-        barcodeReader.startScanning()
+    func onDetected(data: NSArray) {
+        DispatchQueue.main.async {
+                self.channel.invokeMethod("onDetected", arguments: data)
+            }
     }
-
-    func textResultCallback(_ frameId: Int, results: [iTextResult]?, userData: NSObject?) {
-        if results!.count > 0 {
-            var msgText:String = ""
-            for item in results! {
-                if item.barcodeFormat_2.rawValue != 0 {
-                    msgText = msgText + String(format:"\nFormat: %@\nText: %@\n", item.barcodeFormatString_2!, item.barcodeText ?? "noResuslt")
-                }else{
-                    msgText = msgText + String(format:"\nFormat: %@\nText: %@\n", item.barcodeFormatString!,item.barcodeText ?? "noResuslt")
-                }
-            }
-            DispatchQueue.main.async {
-                print(msgText)
-                self.channel.invokeMethod("onDetected", arguments: msgText)
-            }
-        }
-    } 
 }
--- ios/Classes/FLQRCodeScanner.swift
+++ ios/Classes/FLQRCodeScanner.swift
@@ -1,6 +1,92 @@
 import Flutter
 import UIKit
+import DynamsoftBarcodeReader
+import DynamsoftCameraEnhancer
 
-class FLQRCodeScanner {
+public protocol DetectionHandler {
+        func onDetected(data: NSArray)
+    }
 
+class FLQRCodeScanner: NSObject, DBRTextResultDelegate {
+
+    private var cameraView: DCECameraView
+    private var dce: DynamsoftCameraEnhancer
+    private var barcodeReader: DynamsoftBarcodeReader! = nil
+    private var handler: DetectionHandler?
+
+    init(cameraView: DCECameraView, dce: DynamsoftCameraEnhancer) {
+        self.cameraView = cameraView
+        self.cameraView.overlayVisible = true
+        self.dce = dce
+        super.init()
+
+        createBarcodeReader(dce: dce)
+    }
+
+    func setDetectionHandler(handler: DetectionHandler) {
+        self.handler = handler;
+    }
+
+    func createBarcodeReader(dce: DynamsoftCameraEnhancer) {
+        // To activate the sdk, apply for a license key: https://www.dynamsoft.com/customer/license/trialLicense?product=dbr
+        barcodeReader = DynamsoftBarcodeReader.init(license: "license-key")
+        barcodeReader.setCameraEnhancer(dce)
+
+        // Set text result call back to get barcode results.
+        barcodeReader.setDBRTextResultDelegate(self, userData: nil)
+
+        // Start the barcode decoding thread.
+        barcodeReader.startScanning()
+    }
+
+    func textResultCallback(_ frameId: Int, results: [iTextResult]?, userData: NSObject?) {
+        if results!.count > 0 {
+            let outResults = NSMutableArray(capacity: 8)
+            for item in results! {
+                let subDic = NSMutableDictionary(capacity: 8)
+                if item.barcodeFormat_2 != EnumBarcodeFormat2.Null {
+                    subDic.setObject(item.barcodeFormatString_2 ?? "", forKey: "format" as NSCopying)
+                }else{
+                    subDic.setObject(item.barcodeFormatString ?? "", forKey: "format" as NSCopying)
+                }
+                subDic.setObject(item.barcodeText ?? "", forKey: "text" as NSCopying)
+                let points = item.localizationResult?.resultPoints as! [CGPoint]
+                subDic.setObject(Int(points[0].x), forKey: "x1" as NSCopying)
+                subDic.setObject(Int(points[0].x), forKey: "y1" as NSCopying)
+                subDic.setObject(Int(points[1].x), forKey: "x2" as NSCopying)
+                subDic.setObject(Int(points[1].x), forKey: "y2" as NSCopying)
+                subDic.setObject(Int(points[2].x), forKey: "x3" as NSCopying)
+                subDic.setObject(Int(points[2].x), forKey: "y3" as NSCopying)
+                subDic.setObject(Int(points[3].x), forKey: "x4" as NSCopying)
+                subDic.setObject(Int(points[3].x), forKey: "y4" as NSCopying)
+                subDic.setObject(item.localizationResult?.angle ?? 0, forKey: "angle" as NSCopying)
+                outResults.add(subDic)
+            }
+            
+            if handler != nil {
+                handler!.onDetected(data: outResults)
+            }
+        }
+    } 
+
+    func startScan() {
+        cameraView.overlayVisible = true
+        barcodeReader.startScanning()
+    }
+
+    func stopScan() {
+        cameraView.overlayVisible = false
+        barcodeReader.stopScanning()
+    }
+
+    func setBarcodeFormats(arg:NSDictionary) {
+        let formats:Int = arg.value(forKey: "formats") as! Int
+        let settings = try! barcodeReader!.getRuntimeSettings()
+        settings.barcodeFormatIds = formats
+        barcodeReader!.update(settings, error: nil)
+    }
+
+    func setLicense(license: String) {
+        barcodeReader.license = license
+    }
 }
